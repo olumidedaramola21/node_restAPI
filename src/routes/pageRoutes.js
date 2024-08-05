053530118cc49b9f6498e889027b56decc63725b
Olumide <olumidedaramola426@gmail.com>
2024-08-05 05:49:03 +0100

    update

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -1,5 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
 
-module.exports = router 
+module.exports = router;
